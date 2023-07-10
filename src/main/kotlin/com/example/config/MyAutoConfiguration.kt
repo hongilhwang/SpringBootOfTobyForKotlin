50a1d37b2a795751a3ce3639eac362e851e063a0
steven.hwang <steven.hwang@kakacorp.com>
2023-07-10 13:36:27 +0900

    ImportSelector를 이용하여 Import 정보를 파일에 저장하여 로드하기

---
+++ src/main/kotlin/com/example/config/MyAutoConfiguration.kt
@@ -0,0 +1,8 @@
+package com.example.config
+
+import org.springframework.context.annotation.Configuration
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.TYPE)
+@Configuration
+annotation class MyAutoConfiguration
