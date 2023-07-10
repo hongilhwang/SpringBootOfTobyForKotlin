925cd8fa72721fabde510304eebcb0e56068cbd2
steven.hwang <steven.hwang@kakacorp.com>
2023-07-10 13:42:28 +0900

    섹션6 자동 구성 기반 애플리케이션 > 자동 구성 애노테이션 적용

--- src/main/kotlin/com/example/config/MyAutoConfiguration.kt
+++ src/main/kotlin/com/example/config/MyAutoConfiguration.kt
@@ -3,6 +3,6 @@ package com.example.config
 import org.springframework.context.annotation.Configuration
 
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.TYPE)
+@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
 @Configuration
 annotation class MyAutoConfiguration
